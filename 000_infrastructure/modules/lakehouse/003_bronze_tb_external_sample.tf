46a223c7d1f2ed064789d21e68c3e0df331f80e6
Guilherme S. Magalhães <40049979+Guisilcol@users.noreply.github.com>
2025-09-29 14:38:28 -0300

    Update 003_bronze_tb_external_sample.tf

---
+++ 000_infrastructure/modules/lakehouse/003_bronze_tb_external_sample.tf
@@ -0,0 +1,33 @@
+resource "google_bigquery_table" "bronze_tb_external_sample" {
+  project           = var.project_id
+  dataset_id        = google_bigquery_dataset.bronze_dataset.dataset_id
+  deletion_protection = false
+  table_id          = "tb_external_sample"
+
+  external_data_configuration {
+    autodetect      = false
+    source_format   = "PARQUET"
+
+    source_uris     = ["gs://${google_storage_bucket.bronze_bucket.name}/tb_external_sample"]
+
+    hive_partitioning_options {
+      mode              = "STRINGS"
+      source_uri_prefix = "gs://${google_storage_bucket.bronze_bucket.name}/tb_external_sample"
+    }
+  }
+
+  schema = <<EOF
+[
+  {
+    "name": "id",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "data",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  }
+]
+EOF
+}
