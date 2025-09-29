a00db1d2db6ec56826c65b17e5641c80c743c690
Guilherme S. Magalhães <40049979+Guisilcol@users.noreply.github.com>
2025-09-29 13:45:06 -0300

    Update bucket names to remove project ID suffix
    Bucket names in dev.tfvars and 001_buckets.tf have been updated to use explicit names without appending the project ID. This change ensures bucket names are consistent and match the new naming convention.

--- 000_infrastructure/modules/lakehouse/001_buckets.tf
+++ 000_infrastructure/modules/lakehouse/001_buckets.tf
@@ -1,26 +1,26 @@
 resource "google_storage_bucket" "reception_bucket" {
-  name          = "${var.reception_bucket_name}-${var.project_id}"
+  name          = "${var.reception_bucket_name}"
   project       = var.project_id
   location      = var.region
   force_destroy = true
 }
 
 resource "google_storage_bucket" "bronze_bucket" {
-  name          = "${var.bronze_bucket_name}-${var.project_id}"
+  name          = "${var.bronze_bucket_name}"
   project       = var.project_id
   location      = var.region
   force_destroy = true
 }
 
 resource "google_storage_bucket" "silver_bucket" {
-  name          = "${var.silver_bucket_name}-${var.project_id}"
+  name          = "${var.silver_bucket_name}"
   project       = var.project_id
   location      = var.region
   force_destroy = true
 }
 
 resource "google_storage_bucket" "gold_bucket" {
-  name          = "${var.gold_bucket_name}-${var.project_id}"
+  name          = "${var.gold_bucket_name}"
   project       = var.project_id
   location      = var.region
   force_destroy = true
