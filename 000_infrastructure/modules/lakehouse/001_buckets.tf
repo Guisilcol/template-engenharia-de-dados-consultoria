37090f808c8299900e9da9bd8b5e42d15ec98d28
Guilherme S. Magalhães <40049979+Guisilcol@users.noreply.github.com>
2025-09-29 13:41:27 -0300

    fix: ajuste na nomenclatura dos buckets

--- 000_infrastructure/modules/lakehouse/001_buckets.tf
+++ 000_infrastructure/modules/lakehouse/001_buckets.tf
@@ -1,26 +1,26 @@
 resource "google_storage_bucket" "reception_bucket" {
-  name          = "${var.project_id}-${var.reception_bucket_name}"
+  name          = "${var.reception_bucket_name}-${var.project_id}"
   project       = var.project_id
   location      = var.region
   force_destroy = true
 }
 
 resource "google_storage_bucket" "bronze_bucket" {
-  name          = "${var.project_id}-${var.bronze_bucket_name}"
+  name          = "${var.bronze_bucket_name}-${var.project_id}"
   project       = var.project_id
   location      = var.region
   force_destroy = true
 }
 
 resource "google_storage_bucket" "silver_bucket" {
-  name          = "${var.project_id}-${var.silver_bucket_name}"
+  name          = "${var.silver_bucket_name}-${var.project_id}"
   project       = var.project_id
   location      = var.region
   force_destroy = true
 }
 
 resource "google_storage_bucket" "gold_bucket" {
-  name          = "${var.project_id}-${var.gold_bucket_name}"
+  name          = "${var.gold_bucket_name}-${var.project_id}"
   project       = var.project_id
   location      = var.region
   force_destroy = true
