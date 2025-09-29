e477c8127dfa3225d28a9763ff4feb17df56c78c
Guilherme S. Magalhães <40049979+Guisilcol@users.noreply.github.com>
2025-09-29 13:23:52 -0300

    feat: Desabilitar proteção contra exclusão para tabela bronze_tb_sample
    Adicionado deletion_protection=false ao recurso google_bigquery_table.bronze_tb_sample para permitir que a tabela seja excluída quando necessário.

--- 000_infrastructure/modules/lakehouse/003_bronze_tb_sample.tf
+++ 000_infrastructure/modules/lakehouse/003_bronze_tb_sample.tf
@@ -1,6 +1,7 @@
 resource "google_bigquery_table" "bronze_tb_sample" {
   project    = var.project_id
   dataset_id = google_bigquery_dataset.bronze_dataset.dataset_id
+  deletion_protection=false
   table_id   = "tb_sample"
 
   schema = <<EOF
