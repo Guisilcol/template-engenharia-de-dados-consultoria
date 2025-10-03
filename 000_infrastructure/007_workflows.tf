3ea9ab2f180a264aa272584a9d2b9b0399bcc9eb
Guilherme S. Magalhães <40049979+Guisilcol@users.noreply.github.com>
2025-10-02 22:35:50 -0300

    Update service account reference in workflow module
    Changed the service account email from 'cloudrun_service_account' to 'cloud_run_job_service_account' in the sample_workflow module to use the correct service account for workflow execution.

--- 000_infrastructure/007_workflows.tf
+++ 000_infrastructure/007_workflows.tf
@@ -4,7 +4,7 @@ module "sample_workflow" {
   name                  = "sample-workflow"
   location              = var.region
   project_id            = var.project_id
-  service_account_email = google_service_account.cloudrun_service_account.email
+  service_account_email = google_service_account.cloud_run_job_service_account.email
   workflow_yaml_path    = "${path.module}/../002_orquestration/001_sample_workflow.yaml"
 
   # Variables to be replaced in the YAML template using templatefile
