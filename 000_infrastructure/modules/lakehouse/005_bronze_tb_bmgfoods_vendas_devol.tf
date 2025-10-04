724370e6f9d8d463ade19c1a27497eddb8a3c12f
Guilherme S. Magalhães <40049979+Guisilcol@users.noreply.github.com>
2025-10-04 18:19:21 -0300

    Add BigQuery table for bmgfoods vendas devoluções
    Creates a new BigQuery external table 'tb_bmgfoods_vendas_devol' in the bronze dataset, configured to read Parquet files from the bronze bucket with a defined schema and hive partitioning options.

---
+++ 000_infrastructure/modules/lakehouse/005_bronze_tb_bmgfoods_vendas_devol.tf
@@ -0,0 +1,129 @@
+resource "google_bigquery_table" "bronze_tb_bmgfoods_vendas_devol" {
+  project             = var.project_id
+  dataset_id          = google_bigquery_dataset.bronze_dataset.dataset_id
+  deletion_protection = false
+  table_id            = "tb_bmgfoods_vendas_devol"
+
+  external_data_configuration {
+    autodetect    = false
+    source_format = "PARQUET"
+
+    source_uris = ["gs://${google_storage_bucket.bronze_bucket.name}/bmgfoods_vendas_devol/*"]
+
+    hive_partitioning_options {
+      mode                     = "STRINGS"
+      source_uri_prefix        = "gs://${google_storage_bucket.bronze_bucket.name}/bmgfoods_vendas_devol"
+      require_partition_filter = false
+    }
+  }
+
+  schema = <<EOF
+[
+  {
+    "name": "apelido",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "idfilial",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "tipo",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "data_emissao",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "documento",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "idemitente",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "razao_social",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "cnpj_cpf",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "grupo",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "subgrupo",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "idproduto",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "idindustria",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "produto",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "tipo_preco",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "quantidade",
+    "type": "FLOAT64",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "peso_liquido",
+    "type": "FLOAT64",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "valor_total",
+    "type": "FLOAT64",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "valor_unitario",
+    "type": "FLOAT64",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "estabelecimento",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "arquivo_origem",
+    "type": "STRING",
+    "mode": "NULLABLE"
+  },
+  {
+    "name": "data_criacao",
+    "type": "DATE",
+    "mode": "NULLABLE"
+  }
+]
+EOF
+}
