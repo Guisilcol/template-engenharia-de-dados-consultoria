b5ddc00e34289b105c14a41aa9a585163ea51598
Guilherme S. Magalhães <40049979+Guisilcol@users.noreply.github.com>
2025-10-11 01:11:42 -0300

    fix: adicionada dependencia direta das alterações das API para evitar conflitos no deploy de secrets

--- 000_infrastructure/008_secrets.tf
+++ 000_infrastructure/008_secrets.tf
@@ -1,48 +1,54 @@
 
 module "some_secret" {
-    source = "./modules/secret_manager"
+  source = "./modules/secret_manager"
 
-    project_id = var.project_id
-    secrets = [
-        {
-            secret_id  = "my-secret"
-            description = "This is a secret created by Terraform"
-        }
-    ]
+  project_id = var.project_id
+  secrets = [
+    {
+      secret_id   = "my-secret"
+      description = "This is a secret created by Terraform"
+    }
+  ]
+
+  depends_on = [google_project_service.required_apis]
 }
 
 # Secret para armazenar o HMAC Access ID
 module "hmac_access_id_secret" {
-    source = "./modules/secret_manager"
+  source = "./modules/secret_manager"
 
-    project_id   = var.project_id
-    secret_value = google_storage_hmac_key.cloud_run_job_hmac_key.access_id
-    secrets = [
-        {
-            secret_id   = "cloud-run-job-hmac-access-id"
-            description = "HMAC Access ID for cloud_run_job_service_account"
-            labels = {
-                service_account = "cloud-run-job"
-                type           = "hmac-access-id"
-            }
-        }
-    ]
+  project_id   = var.project_id
+  secret_value = google_storage_hmac_key.cloud_run_job_hmac_key.access_id
+  secrets = [
+    {
+      secret_id   = "cloud-run-job-hmac-access-id"
+      description = "HMAC Access ID for cloud_run_job_service_account"
+      labels = {
+        service_account = "cloud-run-job"
+        type            = "hmac-access-id"
+      }
+    }
+  ]
+
+  depends_on = [google_project_service.required_apis]
 }
 
 # Secret para armazenar o HMAC Secret
 module "hmac_secret_key_secret" {
-    source = "./modules/secret_manager"
+  source = "./modules/secret_manager"
 
-    project_id   = var.project_id
-    secret_value = google_storage_hmac_key.cloud_run_job_hmac_key.secret
-    secrets = [
-        {
-            secret_id   = "cloud-run-job-hmac-secret"
-            description = "HMAC Secret Key for cloud_run_job_service_account"
-            labels = {
-                service_account = "cloud-run-job"
-                type           = "hmac-secret"
-            }
-        }
-    ]
+  project_id   = var.project_id
+  secret_value = google_storage_hmac_key.cloud_run_job_hmac_key.secret
+  secrets = [
+    {
+      secret_id   = "cloud-run-job-hmac-secret"
+      description = "HMAC Secret Key for cloud_run_job_service_account"
+      labels = {
+        service_account = "cloud-run-job"
+        type            = "hmac-secret"
+      }
+    }
+  ]
+
+  depends_on = [google_project_service.required_apis]
 }
